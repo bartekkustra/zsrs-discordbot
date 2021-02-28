c96d0aaa6d7cd1c846608c2c97451ab8eb96721f
Bartosz Bart Kustra <bartekkustra@gmail.com>
2021-02-28 18:48:42 +0100

    add !help command

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,9 +1,11 @@
 import { checkForEdyta } from './edyta'
 import { checkForUrl } from './url'
+import { helpCommand } from './help'
 
 const features = {
   checkForEdyta,
   checkForUrl,
+  helpCommand,
 }
 
 export default features
