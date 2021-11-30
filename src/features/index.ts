84a5c56201bb147fb58f199023c86f958ff9189f
Bartosz Bart Kustra <bartekkustra@gmail.com>
2021-11-30 22:10:42 +0100

    remove edyta and czarek

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,17 +1,11 @@
-import { checkForCzarek } from './czarek'
-import { checkForEdyta } from './edyta'
 import { checkForUrl } from './url'
 import { gamesBot } from './games'
 import { helpCommand } from './help'
-import { szczepienie } from './szczepienie'
 
 const features = {
-  checkForEdyta,
-  checkForCzarek,
   checkForUrl,
   gamesBot,
   helpCommand,
-  szczepienie,
 }
 
 export default features
